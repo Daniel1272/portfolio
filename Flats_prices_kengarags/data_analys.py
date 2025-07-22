bf1e6b980e33f776fa702b21726571a4380b0728
Daniel1272 <118887743+Daniel1272@users.noreply.github.com>
2025-07-22 13:45:47 +0300

    1.1

--- Flats_prices_kengarags/data_analys.py
+++ Flats_prices_kengarags/data_analys.py
@@ -89,7 +89,7 @@ ax4.plot(filtered['square_meters'], prediction, color='red')
 
 
 plt.tight_layout()
-plt.savefig('flats_prices_analysis.png')
+plt.savefig('flats_prices_analysis.png',dpi=300)
 
 
 
